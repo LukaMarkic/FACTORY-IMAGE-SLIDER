3ad0936734048c261c984ffdeaa7d9fd888bb4e4
LukaMarkic <73442744+LukaMarkic@users.noreply.github.com>
2023-09-08 21:17:33 +0200

    Adding jQuery

--- scripts/slider-script.js
+++ scripts/slider-script.js
@@ -1,70 +1,73 @@
-const sliderTop = document.querySelector(".slider-top-row");
-const topSliderImages = document.querySelectorAll(".slider-top-row img");
-const sliderBottom = document.querySelector(".slider-bottom-row");
-const bottomSliderImages = document.querySelectorAll(".slider-bottom-row img");
-
-const nextButton =  document.querySelector('#nav-button-right');
-const backButton = document.querySelector('#nav-button-left');
-
-let currentTopSliderIndex = 0, previousTopSliderIndex;
-let currentBottomSliderIndex = 0, previousBottomSliderIndex;
-
-const numberOfTopSliderImages = topSliderImages.length;
-const numberOfBottomSliderImages = bottomSliderImages.length;
-const removeTimeout = 490;
-const sliderGapSize = 10;
-
-nextButton.addEventListener("click", () => {
-    previousTopSliderIndex = currentTopSliderIndex;
-    currentTopSliderIndex = (currentTopSliderIndex - 1 + numberOfTopSliderImages) % numberOfTopSliderImages;
-
-    previousBottomSliderIndex = currentBottomSliderIndex;
-    currentBottomSliderIndex = (currentBottomSliderIndex - 1 + numberOfBottomSliderImages) % numberOfBottomSliderImages;
-
-    scrollRight(topSliderImages, currentTopSliderIndex, sliderTop);
-    scrollRight(bottomSliderImages, currentBottomSliderIndex, sliderBottom);
-   
-});
-
-backButton.addEventListener("click", () => {
-    previousTopSliderIndex = currentTopSliderIndex;
-    currentTopSliderIndex = (currentTopSliderIndex + 1) % numberOfTopSliderImages;
-    previousBottomSliderIndex = currentBottomSliderIndex;
-    currentBottomSliderIndex = (currentBottomSliderIndex + 1) % numberOfBottomSliderImages;
-
-    scrollLeft(topSliderImages, previousTopSliderIndex, sliderTop);
-    scrollLeft(bottomSliderImages, previousBottomSliderIndex, sliderBottom);
-
-});
-
-
-function scrollRight(sliderImages, currentIndex, slider){
-    
-    sliderImages.forEach(img => img.style.transform = `translateX(${sliderImages[currentIndex].clientWidth + sliderGapSize}px)`);
-    slider.insertBefore(sliderImages[currentIndex], slider.firstChild);
-
-    setTimeout(() => {
-        sliderImages.forEach(img => img.style.transform = "");
-        sliderImages.forEach(img => img.classList.add("sliding-transition"));
-    }, 10);
-
-    setTimeout(() => {
-        sliderImages.forEach(img => img.classList.remove("sliding-transition"));
-    }, removeTimeout);
-}
-
-function scrollLeft(sliderImages, previousIndex, slider){
-    sliderImages.forEach(img => img.classList.add("sliding-transition"));
-    sliderImages.forEach(img => img.style.transform = `translateX(${sliderImages[previousIndex].clientWidth + sliderGapSize}px)`);
+$(document).ready(function() {
+    const sliderTop = $(".slider-top-row");
+    const topSliderImages = $(".slider-top-row img");
+    const sliderBottom = $(".slider-bottom-row");
+    const bottomSliderImages = $(".slider-bottom-row img");
   
-    setTimeout(() => {
-        slider.appendChild(sliderImages[previousIndex]);
-        sliderImages.forEach(img => img.classList.remove("sliding-transition"));
-        sliderImages.forEach(img => img.style.transform = "");
-    }, removeTimeout);
-}
-
-
-
-
-
+    const rightButton = $("#nav-button-right");
+    const leftButton = $("#nav-button-left");
+  
+    let currentTopSliderIndex = 0, previousTopSliderIndex;
+    let currentBottomSliderIndex = 0, previousBottomSliderIndex;
+  
+    const numberOfTopSliderImages = topSliderImages.length;
+    const numberOfBottomSliderImages = bottomSliderImages.length;
+    const removeTimeout = 490;
+    const sliderGapSize = 10;
+  
+    rightButton.click(() => {
+        previousTopSliderIndex = currentTopSliderIndex;
+        currentTopSliderIndex = (currentTopSliderIndex + 1) % numberOfTopSliderImages;
+        previousBottomSliderIndex = currentBottomSliderIndex;
+        currentBottomSliderIndex = (currentBottomSliderIndex - 1 + numberOfBottomSliderImages) % numberOfBottomSliderImages;
+  
+        scrollRight(topSliderImages, previousTopSliderIndex, sliderTop);
+        scrollRight(bottomSliderImages, previousBottomSliderIndex, sliderBottom);
+    });
+  
+    leftButton.click(() => {
+        previousTopSliderIndex = currentTopSliderIndex;
+        currentTopSliderIndex = (currentTopSliderIndex - 1 + numberOfTopSliderImages) % numberOfTopSliderImages;
+        previousBottomSliderIndex = currentBottomSliderIndex;
+        currentBottomSliderIndex = (currentBottomSliderIndex + 1) % numberOfBottomSliderImages;
+  
+        scrollLeft(topSliderImages, currentTopSliderIndex, sliderTop);
+        scrollLeft(bottomSliderImages, currentBottomSliderIndex, sliderBottom);
+    });
+  
+    function scrollLeft(sliderImages, currentIndex, slider) {
+        $(sliderImages[currentIndex]).addClass("fade-in-animation");
+        sliderImages.each(function(index, img) {
+            $(img).css("transform", `translateX(${sliderImages[currentIndex].clientWidth + sliderGapSize}px)`);
+        });
+         slider.prepend(sliderImages[currentIndex]);
+  
+        setTimeout(function() {
+            sliderImages.each(function(index, img) {
+                $(img).css("transform", "");
+                $(img).addClass("sliding-transition");
+            });
+        }, 10);
+  
+        setTimeout(function() {
+            sliderImages.removeClass("sliding-transition");
+            $(sliderImages[currentIndex]).removeClass("fade-in-animation");
+        }, removeTimeout);
+    }
+  
+    function scrollRight(sliderImages, previousIndex, slider) {
+        sliderImages.addClass("sliding-transition");
+        sliderImages.each(function(index, img) {
+            $(img).css("transform", `translateX(${sliderImages[previousIndex].clientWidth + sliderGapSize}px)`);
+        });
+    
+        setTimeout(function() {
+            slider.append(sliderImages[previousIndex]);
+            sliderImages.removeClass("sliding-transition");
+            sliderImages.each(function(index, img) {
+            $(img).css("transform", "");
+            });
+        }, removeTimeout);
+    }
+  });
+  
